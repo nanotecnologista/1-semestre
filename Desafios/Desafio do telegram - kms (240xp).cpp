2fdbcba0b6e5f93bf8c6d6833349bba7522884f7
Juju Nanotech <julia.ingridsantos.7@gmail.com>
2021-05-01 18:04:08 -0300

    nova pasta

---
+++ Desafios/Desafio do telegram - kms (240xp).cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+           
+            /*             _
+                         / /
+               __   __  '-'__  ___     ___
+              |  | |  |   |  ||   |   |   |
+              |  | |  |   |  ||   |___|   |
+         __   |  | |  |   |  ||   '___'   |
+        |  |__|  | |   ---   ||   |   |   | 
+         \__ __ _|  \_______/ '___'   '___' */
+
+      /* Desafio km*/
+      
+      
+
+int main (){
+
+   float km, valor;
+   
+   printf (" Qual dist. vai percorrer? \n");
+   scanf ("%f", &km);
+   
+   if (km<200){
+       valor= km*0.50;
+       printf ("O valor cobrado sera %.2f \n", valor);
+   }else{
+       valor= km*0.45;
+       printf ("O valor cobrado sera %.2f \n", valor);
+   }
+
+   system ("PAUSE");
+   return (0);
+
+}
